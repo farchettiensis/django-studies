187153e72ad74a4e64bae1331399d10ab0a7d6d1
farchettiensis <farchettiensis@users.noreply.github.com>
2023-07-27 02:25:10 -0300

    Views, models, urls for the polls app

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
+from .models import Question
 
 # Create your views here.
+# This code loads the template called polls/index.html and passes it a context. The context is a dictionary mapping template variable names to Python objects.
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     template = loader.get_template("polls/index.html")
+#     context = {
+#         "latest_question_list": latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
+
 def index(request):
-    return HttpResponse("C'est comme ça.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
+
+def results(request, question_id):
+    response = f"You're looking at the results of question {question_id}."
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
