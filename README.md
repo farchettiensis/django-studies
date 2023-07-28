19d2c3ecc4815e7bed5d19f991700433b82dfc66
farchettiensis <farchettiensis@users.noreply.github.com>
2023-07-27 21:50:07 -0300

    Fixed a couple issues with the variables from the views' functions

---
+++ README.md
@@ -0,0 +1,7 @@
+# Polls
+This is the polls app from Django's official documentation, a tutorial I followed to learn the framework. It essentially focuses on Django's core concepts and functionalities, so the pageslook very raw.
+
+## Technologies
+- Python.
+- Django 4.2.3.
+- HTML.
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -34,7 +34,7 @@ def detail(request, question_id):
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"response": question})
+    return render(request, "polls/results.html", {"question": question})
 
 # race condition error possibility
 def vote(request, question_id):
@@ -53,5 +53,5 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse("polls:results", args=(question.id,))) # Always return an HttpResponseRedirect after successfully dealing with POST data. This prevents data from being posted twice if a user hits the Back button.
+        return HttpResponseRedirect(reverse("polls:results", args=[question.id]))
 
