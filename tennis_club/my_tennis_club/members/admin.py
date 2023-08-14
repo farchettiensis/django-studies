2139ca4ca849cc7828252ef008cf2d4b3e65d6fe
farchettiensis <farchettiensis@users.noreply.github.com>
2023-08-14 17:20:45 -0300

    Slug field added to the model

--- tennis_club/my_tennis_club/members/admin.py
+++ tennis_club/my_tennis_club/members/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Member
 
 class MemberAdmin(admin.ModelAdmin):
-    list_display = ("firstname", "lastname", "joined_date",)
-
+  list_display = ("firstname", "lastname", "joined_date",)
+  prepopulated_fields = {"slug": ("firstname", "lastname")}
+  
 admin.site.register(Member, MemberAdmin)
