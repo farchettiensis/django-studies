89a9eec1374f11bad409b5d743faeb5c873f1f40
farchettiensis <farchettiensis@users.noreply.github.com>
2023-08-14 11:45:01 -0300

    Database changed to MySQL

--- tennis_club/my_tennis_club/my_tennis_club/settings.py
+++ tennis_club/my_tennis_club/my_tennis_club/settings.py
@@ -62,10 +62,21 @@ WSGI_APPLICATION = 'my_tennis_club.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysql',
+        'USER': 'root',
+        'PASSWORD': 'kingterry@2018',
+        'HOST': '150.230.76.15',
+        'PORT': '3306',
     }
 }
 
