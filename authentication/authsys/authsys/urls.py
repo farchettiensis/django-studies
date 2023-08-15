6f68a577cce6363e102a34e49c79d13bf1ca3434
farchettiensis <farchettiensis@users.noreply.github.com>
2023-08-15 17:38:20 -0300

    Authentication and email system project

---
+++ authentication/authsys/authsys/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from user import views as user_view
+from django.contrib.auth import views as auth
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('user.urls')),
+    path('login/', user_view.Login, name='login'),
+    path('logout/', auth.LoginView.as_view(template_name='user/index.html'), name='logout'),
+    path('register/', user_view.register, name='register'),
+]
